816858203718b8afea85e84e2128782606e09c77
maslo34 <maslo3332@yandex.ru>
2025-01-23 01:09:52 +0300

    add minimal functional. input and list task

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import RoutesApp from "./routesApp.tsx"
+import RoutesApp from "./routesApp.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { Container, Col, Row, Form, Button, InputGroup } from "react-bootstrap";
+
 const MainPages = () => {
-  return <div>ToDo List</div>;
+  const [inputValue, setInputValue] = useState("");
+	const [taskList, setTaskList] = useState<string []>([])
+
+
+  return (
+    <Container className="container-fluid heig-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Col>
+          <h1>TO-DO List</h1>
+          <InputGroup>
+						<Form.Control  onChange={(event) => setInputValue(event.target.value)}></Form.Control>
+						<Button onClick={() => setTaskList([...taskList, inputValue])}>Add</Button>
+					</InputGroup>
+          {taskList.length !== 0 ?
+						<ul>
+							{taskList.map((task, index) => <li key={index} >{task}</li>)}
+						</ul>
+						: <></>
+					}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default MainPages;
